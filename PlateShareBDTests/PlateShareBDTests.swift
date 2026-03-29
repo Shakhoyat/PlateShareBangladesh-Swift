b644b7a9712a6668ee5ff0b513392263c780b61a
Shakhoyat <shakoyatsujon@gmail.com>
2026-03-29 12:15:43 +0600

    Refactor code structure for improved readability and maintainability

---
+++ PlateShareBDTests/PlateShareBDTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlateShareBDTests.swift
+//  PlateShareBDTests
+//
+//  Created by Himel on 25/2/26.
+//
+
+import Testing
+@testable import PlateShareBD
+
+struct PlateShareBDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
